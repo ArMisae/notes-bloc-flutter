b69a4393e824af4e4329b21e96c0719c99d0a1fd
ArMisae <arnolrodriguez.ar@gmail.com>
2022-01-03 21:33:02 -0600

    Create hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_bloc/bloc/notes/notes_bloc.dart';
+import 'package:notes_bloc/models/note_model.dart';
 import 'package:notes_bloc/screens/home_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(NoteModelAdapter());
+
+  await Hive.openBox<NoteModel>('notes');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
